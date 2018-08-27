6da0014241121a1fde78f52a9b29f0fb46247f01
toming <tomingtoming@gmail.com>
2018-08-27 22:07:22 +0900

    simplify implementation

--- scalatra-sample/src/main/scala/ScalatraBootstrap.scala
+++ scalatra-sample/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,15 @@
-import com.example.app._
-import org.scalatra._
 import javax.servlet.ServletContext
+import org.scalatra._
+
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new MyScalatraServlet, "/*")
+    context.mount(new SampleScalatraServlet, "/*")
+  }
+}
+
+class SampleScalatraServlet extends ScalatraServlet {
+  get("/hello/:name") {
+    s"Hello, ${params("name")}"
   }
 }
