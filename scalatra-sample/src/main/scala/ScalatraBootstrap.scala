3840a7b746cb3303e9bfd9ec183a42398af4fa30
toming <tomingtoming@gmail.com>
2018-08-27 22:25:42 +0900

    implementation completed at scalatra-sample

--- scalatra-sample/src/main/scala/ScalatraBootstrap.scala
+++ scalatra-sample/src/main/scala/ScalatraBootstrap.scala
@@ -1,7 +1,6 @@
 import javax.servlet.ServletContext
 import org.scalatra._
 
-
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new SampleScalatraServlet, "/*")
